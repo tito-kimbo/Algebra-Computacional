e988dc7a95721a6162a2c2ef721bad5385b78ef9
tito-kimbo <eduriver@ucm.es>
2020-10-14 22:32:28 +0200

    Added quotient structure. Equality testing on equivalence classes is left to implementation for the particular quotient.

--- structures/ideals.py
+++ structures/ideals.py
@@ -14,10 +14,36 @@ class Principal_Ideal(ABC):
         pass
 
 # WIP
-class Quotient:
-
-    def __init__(self,ring,ideal):
-        assert isinstance(ring,Ring) and isinstance(ideal, Principal_Ideal)
+class Quotient(Ring):
+    
+    class QuotientElement(Ring.Element): 
+        """Class representing an element of the ring."""
+        
+        def __init__(self,rep):
+            assert isinstance(rep,Ring.Element)
+            self.rep = rep
+        
+        def __add__(self,other):
+            return QuotientElement(self.rep+other.rep)
+            
+        def __sub__(self,other):
+            return QuotientElement(self.rep-other.rep)
+        
+        def __mul__(self,other):
+            return QuotientElement(self.rep*other.rep)
+        
+        @abstractmethod
+        def __eq__(self,other):
+            pass # Usually it should just be checking for divisibility in the case of a principal ideal
+            
+        def __str__(self):
+            return "["+str(self.rep)+"]"
+            
+        def opp(self):
+            return QuotientElement(self.rep.opp())
+    
+    def __init__(self,ring,generator):
+        assert isinstance(ring,Ring) and isinstance(generator, Ring.Element)
         self.ring = ring
-        self.ideal = ideal   
+        self.ideal = Principal_Ideal(ring,generator)
     
