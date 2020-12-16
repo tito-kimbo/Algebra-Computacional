fe322184a6e2b13800d9a2466a9543b7d3bf699d
pablo-vs <pavill01@ucm.es>
2020-12-16 14:56:03 +0100

    Berlekamp splitting

--- algorithms/factorization.py
+++ algorithms/factorization.py
@@ -80,3 +80,84 @@ def distinct_degree_factorization(f):
         result[g[d].deg()] = g[d]
 
     return result
+
+
+def berlekamp_splitting(f, hs):
+    """
+        f must be squarefree and monic
+        hs must be a basis of ker(phi_f))
+    """
+
+    R = f.ring.ring
+    RX = f.ring
+
+    s = len(hs)
+    if s == 1:
+        return f
+
+    i = 1
+    alpha = R.generator()
+    R_elems = [R.zero] + [alpha**i for i in range(R.order()-1)]
+    while i < s:
+        for a in R_elems:
+            g = gcd(f, hs[i]-RX.build([a]))
+            if g != RX.one and g != f:
+                return g
+        i += 1
+
+    raise ValueError()
+
+def ker_phi_basis(f):
+    """
+        Computes a basis for Ker(Phi_f),
+        where is the endomorphism (Fr - id)
+        in Fq / <f>
+    """
+
+    R = f.ring.ring
+    RX = f.ring
+
+    d = f.deg()
+    q = R.order()
+
+    x = RX.build([R.zero,R.one])
+
+    B = [[R.zero]*i + [R.one] + [R.zero]*(d-1-i) for i in range(d)]
+    M = [((x**(i*q) - x**i) % f).coefs for i in range(d)]
+
+    for i in range(d):
+        if len(M[i]) < d:
+            M[i] += [R.zero] * (d-len(M[i]))
+
+    _gaussian_elimination(B,M,R)
+
+    zeros = [i for i in range(d) if M[i] == [R.zero]*d]
+    return [RX.build(B[i]) for i in zeros]
+
+
+def _gaussian_elimination(B, M, R):
+    """
+        Note: modifies B and M in place
+    """
+
+    l = len(M)
+
+    for i in range(l):
+        # find pivot
+        pivots = [j for j in range(i,l) if M[j][i] != R.zero]
+        if len(pivots) == 0:
+            continue
+        else:
+            p = pivots[0]
+            # swap rows
+            M[p], M[i] = M[i], M[p]
+            B[p], B[i] = B[i], B[p]
+
+            # eliminate
+            for j in pivots[1:]:
+                c = M[j][i] // M[i][i]
+                for k in range(i):
+                    B[j][k] -= c*B[i][k]
+                for k in range(i, l):
+                    M[j][k] -= c*M[i][k]
+                    B[j][k] -= c*B[i][k]
